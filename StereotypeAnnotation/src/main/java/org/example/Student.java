ca0880bc3b9f16eb945d0a200ebbbbf6194ce1f2
to-amit <guptaajuju1995@gmail.com>
2023-08-19 00:04:29 +0530

    FEATURE(12): Stereotype Annnotation

---
+++ StereotypeAnnotation/src/main/java/org/example/Student.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+    @Value("Amit Gupta")
+    private String studentName;
+    @Value("Patna")
+    private String city;
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String
+    toString() {
+        return "Student{" +
+                "studentName='" + studentName + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
