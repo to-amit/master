4ef49806504890ff5b3d4cd1ba58dcf6badc2d21
to-amit <guptaajuju1995@gmail.com>
2023-09-03 20:34:24 +0530

    FEATURE(01): SpringJdbc using config.xml
    Database setup | Configuring Jdbc Template to execute query |
    Inserting data to database using Spring JDBC |
    Update Operation using Spring Jdbc |
    Delete Operation using Spring JDBC |
    Row Mapper Concept | Fetching data using Spring JDBC |
    Selecting Multiple Objects using Spring Jdbc|

---
+++ SpringJDBC1/src/main/java/org/example/Main.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import org.example.dao.StudentDao;
+import org.example.entities.Student;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Starting database");
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        //JdbcTemplate template = context.getBean("jdbcTemplate", JdbcTemplate.class);
+        //insert query
+        //String query = "insert into student(id, name, city) values(?,?,?)";
+        //execute query
+        //int result = template.update(query,3,"Sumit","Turki");
+        StudentDao studentDao = context.getBean("studentDao", StudentDao.class);
+        Student student = new Student();
+//        student.setId(4);
+//        student.setName("Sanoj");
+//        student.setCity("Banka");
+
+        student.setId(2);
+        student.setName("Rajkumar");
+        student.setCity("Mithila");
+
+
+//insert
+//        int result = studentDao.insert(student);
+//update
+//        int result = studentDao.change(student);
+//delete
+//        result = studentDao.delete(4);
+//        Student student1 = studentDao.getStudent(2);
+
+//        System.out.println("number of record insert "+result);
+        List<Student> students = studentDao.getallStudents();
+
+        for (Student s:students){
+            System.out.println(s);
+        }
+    }
+}
---
+++ SpringJDBC1/src/main/java/org/example/dao/StudentImplement.java
@@ -0,0 +1,61 @@
+package org.example.dao;
+
+import org.example.entities.Student;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.util.List;
+
+public class StudentImplement implements StudentDao{
+   private JdbcTemplate jdbcTemplate;
+
+    public JdbcTemplate getJdbcTemplate() {
+        return jdbcTemplate;
+    }
+
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public int insert(Student student) {
+        String query = "insert into student(id, name, city) values(?,?,?)";
+        int r = this.jdbcTemplate.update(query,student.getId(), student.getName(),student.getCity());
+
+        return r;
+    }
+
+    @Override
+    public int change(Student student) {
+        String query = "update student set name=?, city=? where id=?";
+        int r = this.jdbcTemplate.update(query,student.getName(),student.getCity(),student.getId());
+
+        return r;
+    }
+
+    @Override
+    public int delete(int studentId) {
+        String query = "delete from student where id=?";
+        int r = this.jdbcTemplate.update(query, studentId);
+        return r;
+    }
+
+    @Override
+    public Student getStudent(int studentId) {
+        //selecting single student data
+        String query = "select * from student where id=?";
+        RowMapper<Student> rowMapper =  new RowMapperImplemet();
+        Student student = this.jdbcTemplate.queryForObject(query, rowMapper, studentId);
+        return student;
+    }
+
+    @Override
+    public List<Student> getallStudents() {
+       //selecting all students
+        String query = "select * from student";
+        List<Student> students =  this.jdbcTemplate.query(query,new RowMapperImplemet());
+        return students;
+    }
+
+
+}
